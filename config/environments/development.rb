a5eba62d21c7cb122d1262cea118c2baa1d71a4b
koyama-c <koyama@linkedge.jp>
2021-08-07 03:21:32 +0900

    Blocked hostエラー対応

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,8 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # Blocked hostエラーがでたため追加
+  # Rails6から追加されたローカル実行のアプリへのDNSリバインディング攻撃防止機能
+  config.hosts.clear
 end
